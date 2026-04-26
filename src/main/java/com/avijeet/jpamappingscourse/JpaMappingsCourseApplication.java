d9b359bf3b28a4d38bf757285ffa7234b909575f
Avijeet Pandey <avijeetpandey25@gmail.com>
2026-04-26 14:46:39 +0530

    add: init project

---
+++ src/main/java/com/avijeet/jpamappingscourse/JpaMappingsCourseApplication.java
@@ -0,0 +1,13 @@
+package com.avijeet.jpamappingscourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMappingsCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaMappingsCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/avijeet/jpamappingscourse/JpaMappingsCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avijeet.jpamappingscourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMappingsCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
